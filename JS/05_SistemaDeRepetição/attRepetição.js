be97b7613020be9d3cc8daee895c3fc1a94d29fb
riLix16 <rianfelixMDS@gmail.com>
2024-05-14 17:37:25 -0300

    algumas coisas

--- JS/05_SistemaDeRepetição/attRepetição.js
+++ JS/05_SistemaDeRepetição/attRepetição.js
@@ -1,6 +1,15 @@
 let contador = 1;
 while (contador <51){
     
+    console.log(`Valor = ${contador}`);
     contador ++;
-    console.log('Valor = ${contador}');
-}
+};
+
+
+let contador2 = 10;
+do{
+    console.log('Valor ='+ contador2);
+    contador2 --;
+
+
+}while(contador2 = 0);
