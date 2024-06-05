6aec98f50021f4f3746a867af70421d99ed47d60
riLix16 <rianfelixMDS@gmail.com>
2024-06-05 17:53:30 -0300

    categorias

--- JS/09_ManpulandoArrays/attManiArrays.js
+++ JS/09_ManpulandoArrays/attManiArrays.js
@@ -1,18 +1,9 @@
 const funcionarios = [
-    {
-        Nome: 'Wally', Salario: 4800, },
-    {
-        Nome: 'Wallace', Salario: 5200,
-    },
-    {
-        Nome: 'Will', Salario: 3900,
-    },
-    {
-        Nome: 'Wazy', Salario: 4500,
-    },
-    {
-        Nome: 'Wilton', Salario: 5000,
-    },
+    { Nome: 'Wally', Salario: 4800 },
+    { Nome: 'Wallace', Salario: 5200 },
+    { Nome: 'Will', Salario: 3900 },
+    { Nome: 'Wazy', Salario: 4500 },
+    { Nome: 'Wilton', Salario: 5000 }
 ];
 
 funcionarios.forEach((funcionario) => console.log(funcionario));
@@ -24,10 +15,16 @@ const funcionariosComAumento = funcionarios.map((funcionario) => {
     };
 });
 
-console.log(funcionariosComAumento);
-
-let filtroSalario = funcionariosComAumento.filter((filtro) => {
-    return filtro <= 5000;
+const funcionarios5000OuMais = funcionariosComAumento.filter((funcionario) => {
+    return funcionario.Salario >= 5000;
 });
 
-console.log(filtroSalario);
+console.log(funcionarios5000OuMais);
+
+const findFuncionarios = funcionarios5000OuMais
+
+console.log(
+    funcionarios.find((achar) => {
+        return achar.Nome === 'Wally';
+    })
+);
