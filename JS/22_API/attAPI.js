8473b60d38fbd50ca13928a5668a975165d88da8
riLix16 <rianfelixMDS@gmail.com>
2024-06-11 21:26:38 -0300

    att 24

--- JS/22_API/attAPI.js
+++ JS/22_API/attAPI.js
@@ -1,22 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Função para buscar as imagens
-    function fetchCatImages() {
-        fetch("https://api.thecatapi.com/v1/images/search?limit=10")
-        .then(response => response.json())
-        .then(data => {
-            // Exibir as imagens na página
-            const catImagesDiv = document.getElementById("catImages");
-            data.forEach(cat => {
-                const img = document.createElement("img");
-                img.src = cat.url;
-                img.style.maxWidth = "300px"; // ajuste opcional de largura máxima
-                img.style.margin = "10px"; // ajuste opcional de margem
-                catImagesDiv.appendChild(img);
-            });
-        })
-        .catch(error => console.error("Erro ao buscar gatos:", error));
-    }
-
-    // Chamada da função para buscar as imagens ao carregar a página
-    fetchCatImages();
-});
+function getImages() {
+    let url_api = document.getElementById("url_api").innerText.trim();
+    console.log(url_api);
+    fetch(url_api, {
+      method: "GET",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        document.getElementById("imagens-container").innerHTML = "";
+        data.forEach((cat) => {
+          let imagem = `
+            <div class="col-12 col-md-6 col-lg-4 mb-3">
+              <div class="card">
+                <img class="card-img-top" src="${cat.url}" alt="Gato">
+              </div>
+            </div>
+          `;
+          document.getElementById("imagens-container").innerHTML += imagem;
+        });
+      });
+  }
+  
